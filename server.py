99cf37251f30bb04048b26788d0aa0ce14def413
Matezy <mathelipp@gmail.com>
2025-02-23 13:06:00 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+requests_list = []
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.json
+    if 'youtube_url' in data:
+        requests_list.append(data['youtube_url'])
+        return jsonify({"message": "Link mentve!"}), 200
+    return jsonify({"error": "Hibás kérés"}), 400
+
+@app.route('/get_links', methods=['GET'])
+def get_links():
+    if requests_list:
+        return jsonify({"youtube_url": requests_list.pop(0)})
+    return jsonify({"youtube_url": None})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
